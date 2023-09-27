987801c8b8c35b0eb3be922a31aa19de36ee209d
Kerim Guenaydin <Kerim.Guenaydin@buhmann.schule>
2023-09-27 13:18:20 +0200

    UI Base Funktionalität

--- kunze-prüfer/Views/MainWindow.xaml.cs
+++ kunze-prüfer/Views/MainWindow.xaml.cs
@@ -15,34 +15,77 @@ namespace kunze_prüfer
     /// </summary>
     public partial class MainWindow : AdonisUI.Controls.AdonisWindow
     {
+        private int selectedPage;
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void ViewHandler(int selectedIndex)
+        private void ViewHandler()
         {
-            navItem_1.Style = (Style)FindResource("NavItem");
-            navItem_2.Style = (Style)FindResource("NavItem");
-            switch (selectedIndex)
+            Style defaultStyle = this.FindResource("NavItem") as Style;
+            Style selectedStyle = this.FindResource("NavItemSelected") as Style;
+            navItem_1.Style = defaultStyle;
+            navItem_2.Style = defaultStyle;
+            navItem_3.Style = defaultStyle;
+            navItem_4.Style = defaultStyle;
+            navItem_5.Style = defaultStyle;
+            navItem_6.Style = defaultStyle;
+            navItem_7.Style = defaultStyle;
+            navItem_8.Style = defaultStyle;
+            navItem_9.Style = defaultStyle;
+            navItem_10.Style = defaultStyle;
+            navItem_11.Style = defaultStyle;
+            navItem_12.Style = defaultStyle;
+            navItem_13.Style = defaultStyle;
+            switch (selectedPage)
             {
                 case 1:
-                    navItem_1.Style = (Style)FindResource("NavItemSelected");
+                    navItem_1.Style = selectedStyle;
                     break;
                 case 2:
-                    navItem_2.Style = (Style)FindResource("NavItemSelected");
+                    navItem_2.Style = selectedStyle;
+                    break;
+                case 4:
+                    navItem_3.Style = selectedStyle;
+                    break;
+                case 5:
+                    navItem_4.Style = selectedStyle;
+                    break;
+                case 7:
+                    navItem_5.Style = selectedStyle;
+                    break;
+                case 8:
+                    navItem_6.Style = selectedStyle;
+                    break;
+                case 10:
+                    navItem_7.Style = selectedStyle;
+                    break;
+                case 11:
+                    navItem_8.Style = selectedStyle;
+                    break;
+                case 13:
+                    navItem_9.Style = selectedStyle;
+                    break;
+                case 14:
+                    navItem_10.Style = selectedStyle;
+                    break;
+                case 15:
+                    navItem_11.Style = selectedStyle;
+                    break;
+                case 16:
+                    navItem_12.Style = selectedStyle;
+                    break;
+                case 17:
+                    navItem_13.Style = selectedStyle;
                     break;
             }
         }
 
-        private void navItem_1_MouseDown(object sender, MouseButtonEventArgs e)
+        private void listBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            ViewHandler(1);
-        }
-
-        private void navItem_2_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            ViewHandler(2);
+            selectedPage = listBox.SelectedIndex;
+            ViewHandler();
         }
     }
 }
