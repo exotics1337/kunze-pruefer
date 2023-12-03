bd0934a90d0b280f5039f83fe6abd5674fe276c6
Sergio <thetyserzs@gmail.com>
2023-12-03 16:46:35 +0100

    Start of Stammdaten UX

--- kunze-prüfer/Views/Stammdaten/Stammdaten.xaml.cs
+++ kunze-prüfer/Views/Stammdaten/Stammdaten.xaml.cs
@@ -1,12 +1,26 @@
 ﻿namespace kunze_prüfer.Views.Stammdaten
 {
+    using System.Collections.ObjectModel;
     using System.Windows.Controls;
 
     public partial class Stammdaten : UserControl
     {
         public Stammdaten()
         {
+            
             InitializeComponent();
+            this.DataContext = this;
+            Stumm = new ObservableCollection<string>();
+            loadbox();
+
+
+        }
+        //Databinding
+        
+        public ObservableCollection<string> Stumm { get; set; }
+        private void loadbox()
+        {
+            Stumm.Add("test");
         }
     }
 }
