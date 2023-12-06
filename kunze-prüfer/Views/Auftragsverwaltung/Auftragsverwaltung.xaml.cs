dea10f5cfb995319c1b451ffa5b85adc05c4ed98
Exotics <39382543+exotics1337@users.noreply.github.com>
2023-12-06 12:43:47 +0100

    Added Dashboard Details Added StammdatenAnlegen Added Auftragsverwaltuung Added AuftragAnlegen

---
+++ kunze-prüfer/Views/Auftragsverwaltung/Auftragsverwaltung.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System.ComponentModel;
+using System.Windows.Controls;
+
+namespace kunze_prüfer.Views.Auftragsverwaltung
+{
+    public partial class Auftragsverwaltung : UserControl
+    {
+        public Auftragsverwaltung()
+        {
+            InitializeComponent();
+            DataContext = this;
+            CurrentView = new DashboardDetails(0, true);
+            //CurrentStepView = new DashboardStepDetails(0);
+        }
+
+        private int _currentStep;
+        private UserControl _currentView;
+        private UserControl _currentStepView;
+        
+        public UserControl CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                if (_currentView != value)
+                {
+                    _currentView = value;
+                    OnPropertyChanged(nameof(CurrentView));
+                }
+            }
+        }
+        public UserControl CurrentStepView
+        {
+            get { return _currentStepView; }
+            set
+            {
+                if (_currentStepView != value)
+                {
+                    _currentStepView = value;
+                    OnPropertyChanged(nameof(CurrentStepView));
+                }
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void StepHandler(int step)
+        {
+            switch (step)
+            {
+                case 1:
+                    // ...
+                    break;
+                case 2:
+                    // ...
+                    break;
+                case 3:
+                    // ...
+                    break;
+                case 4:
+                    // ...
+                    break;
+                case 5:
+                    // ...
+                    break;
+                case 6:
+                    // ...
+                    break;
+            }
+        }
+    }
+}
--- kunze-prüfer/Views/Dashboard.xaml.cs
+++ kunze-prüfer/Views/Dashboard.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using System.ComponentModel;
+using System.Windows.Controls;
+using kunze_prüfer.Views.Auftragsverwaltung;
 
 namespace kunze_prüfer.Views
 {
@@ -7,6 +9,28 @@ namespace kunze_prüfer.Views
         public Dashboard()
         {
             InitializeComponent();
+            DataContext = this;
+            CurrentView = new DashboardDetails(0);
         }
+        
+        private UserControl _currentView;
+        public UserControl CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                if (_currentView != value)
+                {
+                    _currentView = value;
+                    OnPropertyChanged(nameof(CurrentView));
+                }
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        
     }
 }
